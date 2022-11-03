c0a21d967040b5eb5e0d9e36c393ff795b841771
Jeong Hun Nam <njh3421@gmail.com>
2022-11-03 18:48:15 +0900

    Finished code

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,19 +1,24 @@
 import Head from "next/head";
 import { Box, Container } from "@chakra-ui/react";
 import Navbar from "../navbar";
+import VoxelDog from "../voxel-god";
+import NoSsr from "../no-ssr";
 
-const Main=({children,router})=>{
+const Main = ({ children, router }) => {
   return (
     <Box as="main">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Ernest Copper - HomePage</title>
       </Head>
-      <Navbar path={router.asPath}/>
+      <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
+        <NoSsr>
+          <VoxelDog />
+        </NoSsr>
         {children}
       </Container>
     </Box>
-  )
-}
-export default Main
+  );
+};
+export default Main;
